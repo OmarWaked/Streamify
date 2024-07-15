5bc96e29cb8e0371782832a0c865437af5ef6e97
Omar Waked <46515470+OmarWaked@users.noreply.github.com>
2024-07-14 19:12:30 -0700

    Initial Commit

---
+++ Streamify/StreamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamifyApp.swift
+//  Streamify
+//
+//  Created by Omar Waked on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
